90b72c4eb026f70edd49a1e3e75ba06ed7578a99
Simon Emms <simon@simonemms.com>
2022-11-06 17:27:56 +0000

    feat(openfaas): update help documentation

--- cmd/run.go
+++ cmd/run.go
@@ -38,13 +38,7 @@ var runOpts struct {
 // runCmd represents the run command
 var runCmd = &cobra.Command{
 	Use:   "run",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Run the Pushbullet listener",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		creds := &auth.BasicAuthCredentials{
 			User:     runOpts.Username,
